5f92f35ae9ef9ca4b5a957a824217687577a1594
DavidLeDuy <49779816+DavidLeDuy@users.noreply.github.com>
2019-12-04 10:05:03 +0100

    Add files via upload
    add bash

---
+++ vsc-ext-install.sh
@@ -0,0 +1,8 @@
+code --install-extension kevinkyang.auto-comment-blocks
+code --install-extension exodiusstudios.comment-anchors
+code --install-extension pkief.material-icon-theme
+code --install-extension equinusocio.vsc-material-theme
+code --install-extension esbenp.prettier-vscode
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension oderwat.indent-rainbow
+code --install-extension humao.rest-client
