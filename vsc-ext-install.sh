4f3c21200032209dc016dabc7005b6cbe1b80314
DavidLeDuy <49779816+DavidLeDuy@users.noreply.github.com>
2019-12-17 10:56:44 +0100

    Update vsc-ext-install.sh
    added vscode remote extension

--- vsc-ext-install.sh
+++ vsc-ext-install.sh
@@ -9,3 +9,4 @@ code --install-extension oderwat.indent-rainbow
 code --install-extension humao.rest-client
 code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension mikestead.dotenv
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
